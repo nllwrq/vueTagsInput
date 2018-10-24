e7e83b441c77a44414b08975f1114fc717b87b48
nllwrq <wangfei2017@xinhua.org>
2018-10-24 17:02:38 +0800

    失去焦点的时候，也要生成tag tags 直接为数组的时候规则

--- src/tagsInput.vue
+++ src/tagsInput.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="tags" @click="focusInput">
-            <span class="tag" v-for="tag, index in tags"  @click.stop="" >
-                <label @dblclick="editTag" @blur="editTagOver($event, index)">{{tag[tagKey]}}</label>
+            <span class="tag" v-for="tag,index in tags"  @click.stop="" >
+                <label @dblclick="editTag" @blur="editTagOver($event, index)" @keydown.enter.stop="editTagEnter">{{tagKey?tag[tagKey]:tag}}</label>
                 <a href="javascript:;" class="el-icon-close" @click="removeTag(index)"></a>
             </span>
         <input ref="tagInput"
                v-model="newTagText"
+               :placeholder="placeholder"
                @keydown.delete.stop = "removeTag('last')"
                @keydown.enter.stop = "addTag"
+               @blur = "addTag('blur')"
         />
     </div>
 
@@ -29,12 +31,16 @@
             /*显示标签的指定key值*/
             tagKey: {
                 type: String,
-                default: 'name'
+                default: undefined
             },
             /*是否限制重复value*/
             limitSameValue: {
                 type: Boolean,
                 default: true
+            },
+            placeholder: {
+                type: String,
+                default: ''
             }
         },
         data(){
@@ -42,18 +48,27 @@
                 newTagText: ''
             }
         },
+        mounted(){},
         methods: {
-            addTag(){
+            addTag(type){
                 if (this.limitSameValue && this.isSameValue(this.newTagText)) return;
-                const newObj = {};
-                newObj[this.tagKey] = this.newTagText;
-
-                this.tags.push(newObj)
+                /*
+                 10421 【③提升用户体验】在关键字写完没输入回车情况下，直接点击传稿报没有关键字错误
+                 失去焦点也要添加关键字
+                * */
+                if (type === 'blur' && this.newTagText === '') return;
+                if (this.tagKey){
+                    const newObj = {};
+                    newObj[this.tagKey] = this.newTagText;
+                    this.tags.push(newObj)
+                } else {
+                    this.tags.push(this.newTagText)
+                }
                 this.newTagText = '';
             },
             removeTag(index){
                 if (typeof index  === 'number'){
-                    this.tags.splice(index,1);
+                    this.tags.splice(index, 1);
                 } else {
                     if (this.newTagText.length > 0) return;
                     this.tags.pop();
@@ -63,7 +78,6 @@
                 if (this.readOnly) return;
                 const $label = e.target,
                 $tag = $label.parentElement;
-
                 $label.contentEditable = 'plaintext-only';
                 $label.focus();
                 $tag.dataset.editing = true;
@@ -72,18 +86,30 @@
                 const $this = e.target,
                     newTagText = $this.innerText,
                     $tag = $this.parentElement;
-
                 $this.contentEditable = false;
                 delete $tag.dataset.editing;
                 if (this.limitSameValue && this.isSameValue(newTagText)) return;
-                const  newObj = this.tags[index];
-                newObj[this.tagKey] = newTagText;
-                this.tags.splice(index, 1, newObj)
+                if (this.tagKey){
+                    const newObj = this.tags[index];
+                    newObj[this.tagKey] = newTagText;
+                    this.tags.splice(index, 1, newObj)
+                } else {
+                    this.tags.splice(index, 1, newTagText)
+                }
+            },
+            editTagEnter(e){
+                e.target.blur()
             },
             isSameValue(value){
-                for (let i = 0 ,max = this.tags.length; i < max; i++){
-                    if (this.tags[i][this.tagKey] === value){
-                        return true
+                for (let i = 0, max = this.tags.length; i < max; i++){
+                    if (this.tagKey){
+                        if (this.tags[i][this.tagKey] === value){
+                            return true
+                        }
+                    } else {
+                        if (this.tags[i] === value){
+                            return true
+                        }
                     }
                 }
                 return false
@@ -91,15 +117,15 @@
             focusInput(e){
                 this.$refs.tagInput.focus();
                 e.preventDefault();
+            },
+            inputBlur(){
+                console.log(111)
             }
         }
     }
 </script>
 
 <style scoped lang="scss">
-    @import "reset.css";
-    @import "ele-theme.css";
-
     .tags{
         padding-bottom: 2px ;
         border:1px solid #f1faff;
